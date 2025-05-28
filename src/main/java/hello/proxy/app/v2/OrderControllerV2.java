9aa44bf69a67907c2d81c00a46b297bc37ce52ea
Kimgmltn <Kimgmltn0323@gmail.com>
2025-05-28 22:00:03 +0900

    springboot3.x 버전맞춤

--- src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -1,14 +1,10 @@
 package hello.proxy.app.v2;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RequestMapping
-@ResponseBody
+@RestController
 public class OrderControllerV2 {
 
     private OrderServiceV2 orderService;
